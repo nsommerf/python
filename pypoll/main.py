7aaf59bdfee8fe8ff083519ded5cd25ce161e780
nsommerf <nsommerf@gmail.com>
2020-12-30 23:54:06 -0600

    running

--- pypoll/main.py
+++ pypoll/main.py
@@ -1,58 +1,39 @@
 import os
 import csv
+import numpy as np
 
-​# create the csv variable for file
-bank_csv = os.path.join("..", "Resources", "election_data.csv")
-​
-with open(bank_csv) as csvfile:
+candidate = []
+
+# Create the csv variable for file
+election_csv = os.path.join("Resources", "election_data.csv")
+with open(election_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
-     # Read the header row first (skip this part if there is no header)
-	    csv_header = next(csvfile)
-        print(f"Header: {csv_header}")
+    # Read the header row first (skip this part if there is no header)
+    csv_header = next(csvfile)
+    print(f"Header: {csv_header}")
 
-# Test your function with the following:
-#print(average([1, 5, 9]))
-#print(average(range(11)))
-
-#precip = [float(row[PRECIP]) for row in incsv]
-
-#avg_precip = sum(precip, 0.) / (1 and len(precip))  # prevent div-by-0
-#max_precip = max(precip)
-
-#print(
-#   "Avg precip: {:0.3f} in/day, max precip: {:0.3f} in/day"
-#   .format(avg_precip, max_precip)
-#)
-
-# Define the function and have it accept the 'wrestler_data' as its sole parameter
-def print_percentages(wrestler_data):
-
-# For readability, it can help to assign your values to variables with descriptive names
-    name = str(candidate_data[0])
-    wins = int(wrestler_data[1])
-    losses = int(wrestler_data[2])
-    draws = int(wrestler_data[3])
-
-​
     # Read through each row of data after the header
     for row in csv_reader:
-​       # calculate total profit/loss
-        profitloss = [float(row[1])]
-        totalpl = totalpl + profitloss
-        maxvalue = max(maxvalue, profitloss)
-        minvalue = min(minvalue,profitloss)
-        # Convert row to float and compare to grams of fiber
-        if maxvalue = profitloss:
-            maxdate = row[0]
-         elif minvalue = profitloss:
-             mindate = row[0]   
-# calculate the total number of votes cast
-print(len(profitloss))
-​# print complete list of candidates who received votes
-print(sum(profitloss)
-# The percentage of votes each candidate won
-print(average(profitloss))
-#calculate the total number of votes each candidate won
+        # pull candidate into it's own list
+        candidate.append(row[2])
 
+# Define the function and have it accept the 'wrestler_data' as its sole parameter
+def print_percentages(election_data):
+
+# For readability, it can help to assign your values to variables with descriptive names
+    cname = []
+    totalvotes = len(election_data)
+    print(f"Total Votes:  {totalvotes}")
+    cname = np.unique(election_data)
+    for cand in cname:
+        votes = election_data.count(str(cand))
+        percent = round(int(votes) / int(totalvotes), 2)
+        print(f"{cand} : {votes} {percent}")
+
+# calculate the total number of votes cast
+# print complete list of candidates who received votes
+# The percentage of votes each candidate won
+# calculate the total number of votes each candidate won
 # calculate the winner of the election based on popular vote.
+print_percentages(candidate)
