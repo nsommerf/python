8b563bd0a46fe70e7abcf812c55128556019005d
nsommerf <nsommerf@gmail.com>
2020-12-30 22:13:44 -0600

    running

--- pybank/main.py
+++ pybank/main.py
@@ -1,6 +1,6 @@
 import os
 import csv
-
+profitloss = 0
 maxvalue = 0
 minvalue = 0
 totalpl = 0
@@ -15,7 +15,7 @@ def average(numbers):
     return total / length
 
 # create the csv variable for file
-bank_csv = os.path.join("..", "Resources", "budget_data.csv")
+bank_csv = os.path.join("Resources", "budget_data.csv")
 
 with open(bank_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
@@ -25,7 +25,7 @@ with open(bank_csv) as csvfile:
     print(f"Header: {csv_header}")
     # Read through each row of data after the header
     for row in csv_reader:
-        profitloss = [float(row[1])]
+        profitloss = float(row[1])
         pldata.append(profitloss)
         totalpl = totalpl + profitloss
         maxvalue = max(maxvalue, profitloss)
@@ -37,12 +37,13 @@ with open(bank_csv) as csvfile:
             mindate = str(row[0]) 
 
 #calculate the total number of months included in the dataset
-print(f {len(pldata[])})
-​#calculate the net total amount of "Profit/Losses" over the entire period
-print(f {sum(pldata[])})
+print(f"{len(pldata)}")
+
+#calculate the net total amount of "Profit/Losses" over the entire period
+print(f"{sum(pldata)}")
 #calculate the average of the changes in "Profit/Losses" over the entire period
-print(f {average(pldata[])})
+print(f"{average(pldata)}")
 #calculate the greatest increase in profits (date and amount) over the entire period
-print(f 'Greatest Increase in profits' {maxdate} {maxvalue})
+print(f"Greatest Increase in profits {maxdate} {maxvalue}")
 #calculate he greatest decrease in losses (date and amount) over the entire period
-print(f 'Greatest Increase in profits' {mindate} {minvalue})
+print(f"Greatest Decrease in profits {mindate} {minvalue}")
