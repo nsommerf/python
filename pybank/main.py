62783cf6252f3e9b4b0cc54a813a43dc83eba4f6
nsommerf <nsommerf@gmail.com>
2021-01-04 23:22:20 -0600

    More functionality added

--- pybank/main.py
+++ pybank/main.py
@@ -17,6 +17,9 @@ def average(numbers):
 # create the csv variable for file
 bank_csv = os.path.join("Resources", "budget_data.csv")
 
+# Specify the file to write to
+output_path = os.path.join("Analysis", "analysis.txt")
+
 with open(bank_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
@@ -36,14 +39,42 @@ with open(bank_csv) as csvfile:
         elif minvalue == profitloss:
             mindate = str(row[0]) 
 
+print("Financial Analysis")
+print("-----------------------------")
 #calculate the total number of months included in the dataset
-print(f"{len(pldata)}")
+print(f"Total Months:  {len(pldata)}")
 
 #calculate the net total amount of "Profit/Losses" over the entire period
-print(f"{sum(pldata)}")
+print(f"Total:  {sum(pldata)}")
 #calculate the average of the changes in "Profit/Losses" over the entire period
-print(f"{average(pldata)}")
+print(f"Average Change: {average(pldata)}")
 #calculate the greatest increase in profits (date and amount) over the entire period
-print(f"Greatest Increase in profits {maxdate} {maxvalue}")
+print(f"Greatest Increase in profits: {maxdate} {maxvalue}")
 #calculate he greatest decrease in losses (date and amount) over the entire period
-print(f"Greatest Decrease in profits {mindate} {minvalue}")
+print(f"Greatest Decrease in profits: {mindate} {minvalue}")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as file_obj:
+
+    file_obj.write("Financial Analysis\n")
+    file_obj.write("-----------------------------\n")
+    file_obj.write("Total Months:  ")
+    file_obj.write(str(len(pldata)))
+    file_obj.write("\n")
+    file_obj.write("Total:  ")
+    file_obj.write(str(sum(pldata)))
+    file_obj.write("\n")
+    file_obj.write("Average Change: ")
+    file_obj.write(str(average(pldata)))
+    file_obj.write("\n")
+    file_obj.write("Greatest Increase in profits:  ")
+    file_obj.write(str(maxdate))
+    file_obj.write("  ")
+    file_obj.write(str(maxvalue))
+    file_obj.write("\n")
+    file_obj.write("Greatest Decrease in profits:  ")
+    file_obj.write(str(mindate))
+    file_obj.write("  ")
+    file_obj.write(str(minvalue))
+    file_obj.write("\n")
+    file_obj.close()
--- pypoll/main.py
+++ pypoll/main.py
@@ -6,34 +6,69 @@ candidate = []
 
 # Create the csv variable for file
 election_csv = os.path.join("Resources", "election_data.csv")
+
+# Specify the file to write to
+output_path = os.path.join("Analysis", "analysis.txt")
+
 with open(election_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
     # Read the header row first (skip this part if there is no header)
     csv_header = next(csvfile)
-    print(f"Header: {csv_header}")
+    # print(f"Header: {csv_header}")
 
     # Read through each row of data after the header
     for row in csv_reader:
         # pull candidate into it's own list
         candidate.append(row[2])
 
-# Define the function and have it accept the 'wrestler_data' as its sole parameter
-def print_percentages(election_data):
+def print_lines(file_obb):
+    print("-----------------------------")
+    file_obb.write("-----------------------------\n")
 
-# For readability, it can help to assign your values to variables with descriptive names
+# Define the function and have it accept the 'wrestler_data' as its sole parameter
+def print_percentages(election_data, file_obb):
+
+# For readability, it can help to assign your values to variables with descriptive names 
+    winpercent = 0
+    wincand = ''
     cname = []
-    totalvotes = len(election_data)
-    print(f"Total Votes:  {totalvotes}")
     cname = np.unique(election_data)
     for cand in cname:
         votes = election_data.count(str(cand))
-        percent = round(int(votes) / int(totalvotes), 2)
-        print(f"{cand} : {votes} {percent}")
+        percent = round(int(votes)*100 / int(totalvotes), 2)
+        if (percent > winpercent):
+            wincand = str(cand)
+            winpercent = percent
+        print(f"{cand} : {votes} {percent}%")
+        file_obb.write(str(cand))
+        file_obb.write(":  ")
+        file_obb.write(str(votes))
+        file_obb.write("  ")
+        file_obb.write(str(percent))
+        file_obb.write("%\n")
+    print_lines(file_obb)
+    print(f"Winner:  {wincand}")
+    file_obb.write("Winner: ")
+    file_obb.write(str(wincand))
+    file_obb.write("\n")
 
 # calculate the total number of votes cast
 # print complete list of candidates who received votes
 # The percentage of votes each candidate won
 # calculate the total number of votes each candidate won
 # calculate the winner of the election based on popular vote.
-print_percentages(candidate)
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as file_obj:
+    print("Election Analysis")
+    file_obj.write("Election Analysis\n")
+    print_lines(file_obj)
+    totalvotes = len(candidate)
+    print(f"Total Votes:  {totalvotes}")
+    file_obj.write("Total Votes:  ")
+    file_obj.write(str(totalvotes))
+    file_obj.write("\n")
+    print_lines(file_obj)
+    print_percentages(candidate, file_obj)
+    print_lines(file_obj)
+    file_obj.close()
